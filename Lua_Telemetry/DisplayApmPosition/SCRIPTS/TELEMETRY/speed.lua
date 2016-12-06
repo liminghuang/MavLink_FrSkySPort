b317bb892506df3bf5072f4cee3f82bdf3fc1fc2
athertop <patherton@mail.com>
2016-12-06 17:41:46 +0000

    removing as added debug script by mistake

--- Lua_Telemetry/DisplayApmPosition/SCRIPTS/TELEMETRY/speed.lua
+++
@@ -1,9 +0,0 @@
-	local function run(event)
-		lcd.clear()
-		lcd.drawText(1,1,"GSpd: ",MIDSIZE)
-		lcd.drawNumber(lcd.getLastPos(),1,getValue("GSpd")*10,MIDSIZE+LEFT+PREC1)
-		lcd.drawText(1,20,"ASpd: ",MIDSIZE)
-		lcd.drawNumber(lcd.getLastPos(),20,getValue("ASpd")*10,MIDSIZE+LEFT+PREC1)
-	end
-
-	return {run=run}
